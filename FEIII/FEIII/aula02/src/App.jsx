49e501036b64a0879b854849a09c232dff4b2d95
Pedro Henrique Oliveira <chamusaca@gmail.com>
2023-03-09 02:18:07 -0300

    commit atualizando as pastas e caminhos

--- FEIII/FEIII/aula02/src/App.jsx
+++
@@ -1,4 +0,0 @@
-function PrimeiroComponente () {
-  return (<h1>Meu primeiro Componente</h1>);
-}
-export default PrimeiroComponente;
--- FEIII/FEIII/aula04/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return(
-        <footer>
-            <center>Todos os direitos reservados - 2023</center>
-        </footer>
-    );
-}
-
-export default Footer;
